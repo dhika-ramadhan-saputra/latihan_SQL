9e8f7e4c09c1d2aec91bc04749882698aaa3d704
Dhika Ramadhan Saputra <dhikaramadhansaputra@gmail.com>
2025-01-19 16:43:31 +0700

    terakhir bagian practice problem 1 menit 41:18

--- Basic_SQL.sql
+++ Basic_SQL.sql
@@ -1,1 +1,39 @@
-select * from job_posting_fact
+-- Basic Select --
+select * from job_posting_fact;
+
+select * from job_posting_fact limit 10;
+
+select distinct nerd_role, project_tool 
+from job_posting_fact
+where nerd_role = "Data Analyst";
+
+select distinct nerd_role
+from job_posting_fact;
+
+/*
+ini comment paragraf
+*/
+
+select nerd_role , avg(hours_spent) as avg_hours_spent
+from job_posting_fact
+group by nerd_role
+order by hours_spent desc;
+
+select distinct nerd_role
+from job_posting_fact
+where nerd_role <> "Data Analyst" and nerd_role <> "Senior Data Analyst";
+
+select project_id, nerd_role, hours_spent
+from job_posting_fact
+where hours_spent >= 500
+order by hours_spent desc;
+
+select project_id, nerd_role, hours_spent
+from job_posting_fact
+where hours_spent >= 500 and hours_spent <= 700;
+
+select nerd_role, project_tool
+from job_posting_fact
+where project_tool in ('sql', 'tableau')
+group by nerd_role;
+group by ner
