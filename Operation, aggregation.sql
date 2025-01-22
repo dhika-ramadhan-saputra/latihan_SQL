529499f694838706d54c0915652bdc0669008c41
Dhika Ramadhan Saputra <dhikaramadhansaputra@gmail.com>
2025-01-22 13:43:41 +0700

    terakhir 1:10:4

--- Operation, aggregation.sql
+++ Operation, aggregation.sql
@@ -19,4 +19,10 @@ order by hours_rate desc;
 
 select nerd_role, min(hours_rate) as min, max(hours_rate) as max
 from job_posting_fact
-group by nerd_role
+group by nerd_role;
+
+
+-- Practice 3
+select project_id, hours_spent, hours_rate, (hours_spent * hours_rate) as total_earning, (hours_spent * (hours_rate + 5)) as total_plus_five
+from job_posting_fact
+group by project_id;
