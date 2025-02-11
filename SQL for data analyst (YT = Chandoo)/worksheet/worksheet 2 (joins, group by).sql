7ef4475bf3c989f270b77a37c12610474dc055de
Dhika Ramadhan Saputra <dhikaramadhansaputra@gmail.com>
2025-02-11 16:57:32 +0700

    Alhamdulillah, done semua

--- SQL for data analyst (YT = Chandoo)/worksheet/worksheet 2 (joins, group by).sql
+++ SQL for data analyst (YT = Chandoo)/worksheet/worksheet 2 (joins, group by).sql
@@ -48,4 +48,39 @@ left join people as pe
 left join geo as g
 	on s.GeoID = g.GeoID
 where s.Amount <= 500;
-    
+
+
+SELECT * 
+FROM geo 
+CROSS JOIN people 
+CROSS JOIN products 
+CROSS JOIN sales;
+
+-- group by
+select GeoID, sum(Customers), sum(Amount), sum(Boxes)
+from sales
+group by GeoID;
+
+select
+	g.Geo,
+    sum(s.Customers),
+    sum(s.Amount),
+    sum(s.Boxes)
+from geo as g
+inner join sales as s
+	on g.GeoID = s.GeoID
+group by g.Geo;
+
+select
+	p.Team,
+    pr.Product,
+    sum(s.Customers) as jumlah_customer,
+    sum(s.Amount) as jumlah_amount,
+    sum(s.Boxes) as jumlah_boxes
+from people as p
+inner join sales as s
+	on p.SPID = s.SPID
+inner join products as pr
+	on s.PID = pr.PID
+group by pr.Product
+order by jumlah_amount desc;
